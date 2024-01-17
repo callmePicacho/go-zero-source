f183c8815f07f56e52c45b54c1125d7b03ea5a94
李元月 <17c_t5ykxjfkcn@dingtalk.com>
2024-01-17 19:57:39 +0800

    一致性哈希

--- hash/go-zero/main.go
+++ hash/go-zero/main.go
@@ -7,15 +7,22 @@ import (
 )
 
 func main() {
+	test2()
+}
+
+func test1() {
+	// 创建环
 	dispatcher := hash.NewConsistentHash()
 	nodes := []string{"localhost:8080", "localhost:8081", "localhost:8082"}
 
+	// 添加真实节点
 	for weight, node := range nodes {
 		dispatcher.AddWithWeight(node, weight+10)
 	}
 
 	nodeCount := make(map[string]int64)
 
+	// 尝试请求 1w 次
 	for i := 0; i < 10000; i++ {
 		node, ok := dispatcher.Get(rand.Int())
 		if ok {
@@ -24,4 +31,47 @@ func main() {
 	}
 
 	fmt.Println("请求结果:", nodeCount)
+
+	// 移除真实节点
+	dispatcher.Remove(nodes[0])
+
+	nodeCount = make(map[string]int64)
+
+	// 尝试请求 1w 次
+	for i := 0; i < 10000; i++ {
+		node, ok := dispatcher.Get(rand.Int())
+		if ok {
+			nodeCount[node.(string)]++
+		}
+	}
+
+	fmt.Println("请求结果:", nodeCount)
+}
+
+func test2() {
+	// 按照长度hash，冲突特别大
+	lenHashFunc := func(data []byte) uint64 {
+		return uint64(len(data))
+	}
+
+	dispatcher := hash.NewCustomConsistentHash(100, lenHashFunc)
+	nodes := []string{"localhost:808", "localhost:8081", "localhost:18082"}
+
+	// 添加真实节点
+	for weight, node := range nodes {
+		dispatcher.AddWithWeight(node, weight+10)
+	}
+
+	nodeCount := make(map[string]int64)
+
+	// 尝试请求 1w 次
+	for i := 0; i < 10000; i++ {
+		node, ok := dispatcher.Get(rand.Int())
+		if ok {
+			nodeCount[node.(string)]++
+		}
+	}
+
+	fmt.Println("请求结果:", nodeCount)
+
 }
--- hash/go-zero/source/consistenthash.go
+++ hash/go-zero/source/consistenthash.go
@@ -24,9 +24,9 @@ type (
 	// ConsistentHash 一致性哈希实现
 	ConsistentHash struct {
 		hashFunc Func                            // hash函数
-		replicas int                             // 虚拟节点数量
+		replicas int                             // 用于确定真实节点对应的虚拟节点数量，当AddWithWeight权重为100时，添加该真实节点，同时添加对应replicas数量的虚拟节点
 		keys     []uint64                        // 虚拟节点列表
-		ring     map[uint64][]any                // 虚拟节点到真实节点的映射
+		ring     map[uint64][]any                // 虚拟节点到真实节点的映射，当存在冲突，多个真实节点追加到相同的 key
 		nodes    map[string]lang.PlaceholderType // 真实节点的map，用于快速判断是否存在
 		lock     sync.RWMutex                    // 读写锁
 	}
@@ -71,7 +71,7 @@ func (h *ConsistentHash) AddWithReplicas(node any, replicas int) {
 	// 先删除该真实节点
 	h.Remove(node)
 
-	// 不能超过总的虚拟节点个数
+	// 每次添加真实节点，对应的虚拟节点个数不能超过该值
 	if replicas > h.replicas {
 		replicas = h.replicas
 	}
@@ -88,12 +88,12 @@ func (h *ConsistentHash) AddWithReplicas(node any, replicas int) {
 		hash := h.hashFunc([]byte(nodeRepr + strconv.Itoa(i)))
 		// 添加虚拟节点
 		h.keys = append(h.keys, hash)
-		// 虚拟节点 -> 真实节点
+		// 建立虚拟节点 -> 真实节点
 		// 可能出现哈希冲突，使用链表法解决，追加到相同的切片中
 		h.ring[hash] = append(h.ring[hash], node)
 	}
 
-	// 排序
+	// 对虚拟节点hash值排序，方便快速找到虚拟节点
 	sort.Slice(h.keys, func(i, j int) bool {
 		return h.keys[i] < h.keys[j]
 	})
@@ -101,8 +101,7 @@ func (h *ConsistentHash) AddWithReplicas(node any, replicas int) {
 
 // AddWithWeight 按百分比权重添加节点，权重越高，虚拟节点个数越多
 func (h *ConsistentHash) AddWithWeight(node any, weight int) {
-	// don't need to make sure weight not larger than TopWeight,
-	// because AddWithReplicas makes sure replicas cannot be larger than h.replicas
+	// 最多添加h.replicas个的虚拟节点
 	replicas := h.replicas * weight / TopWeight
 	h.AddWithReplicas(node, replicas)
 }
@@ -119,18 +118,21 @@ func (h *ConsistentHash) Get(v any) (any, bool) {
 
 	// 针对 v 计算得到 hash 值
 	hash := h.hashFunc([]byte(repr(v)))
-	//
+	// 二分法返回第一个大于等于hash值的索引（相当于沿顺时针方向，找到第一个大于等于hash值的虚拟节点）
 	index := sort.Search(len(h.keys), func(i int) bool {
 		return h.keys[i] >= hash
 	}) % len(h.keys)
 
+	// index 虚拟节点的索引
+	// h.keys[index] 找到对应虚拟节点的值
+	// h.ring[h.keys[index]] 找到虚拟节点对应的真实节点切片
 	nodes := h.ring[h.keys[index]]
 	switch len(nodes) {
-	case 0:
+	case 0: // 如果虚拟节点没有对应的真实节点，返回空值
 		return nil, false
-	case 1:
+	case 1: // 如果虚拟节点只有一个对应的真实节点，返回该真实节点
 		return nodes[0], true
-	default:
+	default: // 如果虚拟节点有多个对应的真实节点，使用innerRepr(v)计算得到hash值，再使用该值在虚拟节点对应的切片中找到对应的节点
 		innerIndex := h.hashFunc([]byte(innerRepr(v)))
 		pos := int(innerIndex % uint64(len(nodes)))
 		return nodes[pos], true
@@ -145,7 +147,7 @@ func (h *ConsistentHash) Remove(node any) {
 	h.lock.Lock()
 	defer h.lock.Unlock()
 
-	// 真实节点存在，直接返回
+	// 真实节点不存在，直接返回
 	if !h.containsNode(nodeRepr) {
 		return
 	}
@@ -154,11 +156,14 @@ func (h *ConsistentHash) Remove(node any) {
 	for i := 0; i < h.replicas; i++ {
 		// 计算虚拟节点的哈希值
 		hash := h.hashFunc([]byte(nodeRepr + strconv.Itoa(i)))
-		// 找到哈希值在虚拟节点上的位置
+		// 根据哈希值找到该虚拟节点的下标
 		index := sort.Search(len(h.keys), func(i int) bool { return h.keys[i] >= hash })
+		// 二次校验确实是这个虚拟节点，在虚拟节点切片上删除该虚拟节点
 		if index < len(h.keys) && h.keys[index] == hash {
+			// 使用index之后的元素（index+1）前移一位，覆盖index位置的元素
 			h.keys = append(h.keys[:index], h.keys[index+1:]...)
 		}
+		// 删除虚拟节点到真实节点的映射
 		h.removeRingNode(hash, nodeRepr)
 	}
 
@@ -169,15 +174,18 @@ func (h *ConsistentHash) Remove(node any) {
 func (h *ConsistentHash) removeRingNode(hash uint64, nodeRepr string) {
 	// 校验虚拟节点是否在哈希环中
 	if nodes, ok := h.ring[hash]; ok {
+		// 新建空的切片，保持原容量，等同于：make([]any, 0, len(nodes))
 		newNodes := nodes[:0]
+		// 遍历虚拟节点对应的真实节点列表，重新把非当前的真实节点加入到映射
 		for _, x := range nodes {
 			if repr(x) != nodeRepr {
 				newNodes = append(newNodes, x)
 			}
 		}
+		// 如果虚拟节点还有对应的真实节点，使用新真实节点列表作为映射的 value
 		if len(newNodes) > 0 {
 			h.ring[hash] = newNodes
-		} else {
+		} else { // 否则直接删掉整个映射
 			delete(h.ring, hash)
 		}
 	}
@@ -197,7 +205,6 @@ func (h *ConsistentHash) removeNode(nodeRepr string) {
 	delete(h.nodes, nodeRepr)
 }
 
-// TODO 可以理解为确定node字符串值的序列化方法
 // 在遇到哈希冲突时需要重新对key进行哈希计算
 // 为了减少冲突的概率前面追加了一个质数 prime来减小冲突的概率
 func innerRepr(node any) string {
